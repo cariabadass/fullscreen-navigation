d58c5cbaec7e1f7913a433a0475f76776be28f89
cariabadass <154686124+cariabadass@users.noreply.github.com>
2024-01-06 19:24:18 +0100

    Add files via upload

---
+++ fullscreen-navigation/main.js
@@ -0,0 +1,15 @@
+let toggler = document.querySelector(".toggle");
+let nav = document.querySelector("nav");
+let close = document.querySelector(".close");
+
+toggler.onclick = function () {
+  nav.classList.add("open");
+};
+close.onclick = function () {
+  this.parentElement.classList.remove("open");
+};
+document.onkeyup = function (e) {
+  if (e.key === "Escape") {
+    nav.classList.remove("open");
+  }
+};
